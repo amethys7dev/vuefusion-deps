8396a8c70a764b31b37a0b3c45077f6b6ef83e28
Amethyst <22856826-amethystdev@users.noreply.gitlab.com>
2024-09-15 12:01:22 -0400

    Include stdint

--- CustomByteBuffer/CustomByteBuffer.c
+++ CustomByteBuffer/CustomByteBuffer.c
@@ -1,10 +1,11 @@
-#include <jni.h>
 #include "CustomByteBuffer.h"
+#include <jni.h>
+#include <stdint.h>
 #include <stdlib.h>
 
-JNIEXPORT jobject JNICALL
-Java_helper_CustomByteBuffer_createBuffer(JNIEnv *env, jclass clazz, jlong address, jlong capacity) {
-    void *ptr = (void *)(uintptr_t)address;
+JNIEXPORT jobject JNICALL Java_helper_CustomByteBuffer_createBuffer(
+    JNIEnv *env, jclass clazz, jlong address, jlong capacity) {
+  void *ptr = (void *)(uintptr_t)address;
 
-    return (*env)->NewDirectByteBuffer(env, ptr, capacity);
+  return (*env)->NewDirectByteBuffer(env, ptr, capacity);
 }
